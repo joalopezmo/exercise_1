9d2e81b94379927927b97870583459ad4976bcef
joalopezmo <joalopezmo@uoc.edu>
2023-09-03 22:31:24 +0200

    actividad 1
    Objetivos de actividad 1 cumplidos

---
+++ database/migrations/2023_09_03_202656_create_prestamos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('libro_id');
+            $table->date('fecha_prestamo');
+            $table->date('fecha_devolucion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prestamos');
+    }
+};
